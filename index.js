9350a28785560503bfd5cfbe55376cf290fe50cf
Nathan M <nathanmasol@gmail.com>
2019-05-23 17:49:48 -0500

    Parcial terminado (?)

--- index.js
+++ index.js
@@ -16,22 +16,34 @@ var contador = {
     campeon: 0,
 };
 
-function archivoEscrito() {
-
-    //escritura del archivo
-    fs.writeFileSync('visitas.txt', 'Visitas del home: ' + contador.home+'\nVisitas de Historia del Campeon: ' + contador.campeon+'\nVisitas de los Splash art: ' + contador.splash+',' + '\nFecha/hora de la ultima visita: ' + fecha +'', 'utf8');
-
-    //leer archivo
+function archivoEscrito(texto , numeroPag) {
+    var antiguo='';
+    var visita=0;
+    if(numeroPag == 0){
+        visita = contador.home;
+    } else if(numeroPag == 1){
+        visita = contador.splash;
+    }else if(numeroPag == 2){
+        visita = contador.campeon;
+    }
+     //leer archivo
     fs.readFile('visitas.txt', 'utf8', function (err, data) {
         if (err) throw err;
-        console.log(data);
+        console.log('Data:' +data);
+        antiguo = data.toString();
+        console.log('antiguo: '+antiguo);
+
+         //escritura del archivo
+    fs.writeFileSync('visitas.txt', antiguo+'\n'+texto+visita+' - Fecha/hora: ' + fecha +'', 'utf8'); 
     });
+   
 }
 
 //entrada a pagina home
 app.get('/', function (req, response) {
     contador.home++;
-    archivoEscrito();
+    var texto = 'Visita a Home Numero: '
+    archivoEscrito(texto, 0);
     var contexto = {
         layout: false,
         splash: contador.splash,
@@ -44,7 +56,8 @@ app.get('/', function (req, response) {
 //entrada a pagina Splash arts
 app.get('/splash', function (req, response) {
     contador.splash++;
-    archivoEscrito();
+    var texto = 'Visita a Splash-Arts Numero: '
+    archivoEscrito(texto, 1);
     var contexto = {
         layout: false,
         splash: contador.splash,
@@ -58,7 +71,8 @@ app.get('/splash', function (req, response) {
 //entrada a pagina Campeon
 app.get('/campeon', function (req, response) {
     contador.campeon++;
-    archivoEscrito();
+    var texto = 'Visita a Campeon Numero: ';
+    archivoEscrito(texto, 2);
     var contexto = {
         layout: false,
         splash: contador.splash,
